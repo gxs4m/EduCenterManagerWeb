ff047d92838545894483a4adbbb2ccfab3927e3f
Samuel Giraldo <samgirari@gmail.com>
2025-05-24 21:10:28 -0500

    Cambio variables Courses

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -3,8 +3,8 @@
     public class Courses
     {
         public int Id { get; set; } // Primary Key
-        public string? Name { get; set; } 
-        public string? Description { get; set; } 
+        public string? NombreCurso { get; set; } 
+        public string? Descripcion { get; set; } 
         public int TeachersId { get; set; } // Llave Foranea
         public Teachers? Teachers { get; set; } // Propiedad de navegación Profesores
         public ICollection<Students>? Students { get; set; } // Propiedad de navegación Estudiantes
