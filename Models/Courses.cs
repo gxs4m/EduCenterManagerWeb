9edb9a5088adee16ec4d6df570d74d21d5585047
Samuel Giraldo <samgirari@gmail.com>
2025-05-24 21:13:23 -0500

    Cambio variables Course

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -1,10 +1,13 @@
-﻿namespace EduCenterManagerWeb.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduCenterManagerWeb.Models
 {
     public class Courses
     {
         public int Id { get; set; } // Primary Key
         public string? NombreCurso { get; set; } 
-        public string? Descripcion { get; set; } 
+        public string? Descripcion { get; set; }
+        [ForeignKey("TeachersId")]
         public int TeachersId { get; set; } // Llave Foranea
         public Teachers? Teachers { get; set; } // Propiedad de navegación Profesores
         public ICollection<Students>? Students { get; set; } // Propiedad de navegación Estudiantes
